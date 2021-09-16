7b6bbdf86330c900196e7c32777e8b2361118549
samuelaparker <samuel.a.parker00@gmail.com>
2021-09-16 16:07:53 -0400

    add images

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+import img1 from '../../assets/img/Men_s_Tree_Runners_-_Charcoal__Charcoal_Sole__-_imageAngle.webp'
+import img2 from '../../assets/img/Men_s_Tree_Runners_-_Charcoal__Charcoal_Sole__-_imageBack.webp'
+import img3 from '../../assets/img/Men_s_Tree_Runners_-_Charcoal__Charcoal_Sole__-_imageLeft.webp'
+import img4 from '../../assets/img/Men_s_Tree_Runners_-_Charcoal__Charcoal_Sole__-_imagePair.webp'
+import img5 from '../../assets/img/Men_s_Tree_Runners_-_Charcoal__Charcoal_Sole__-_imageSole.webp'
+import img6 from '../../assets/img/Men_s_Tree_Runners_-_Charcoal__Charcoal_Sole__-_imageTop.webp'
+
 
 
 function Grid({ gridColumns }) {
@@ -8,26 +15,32 @@ function Grid({ gridColumns }) {
     {
       id: 1,
       title: 'Item 1',
+      img: img1,
     },
     {
       id: 2,
       title: 'Item 2',
+      img: img2,
     },
     {
       id: 3,
       title: 'Item 3',
+      img: img3,
     },
     {
       id: 4,
       title: 'Item 4',
+      img: img4,
     },
     {
       id: 5,
       title: 'Item 5',
+      img: img5,
     },
     {
       id: 6,
       title: 'Item 6',
+      img: img6,
     },
   ]
 
@@ -35,8 +48,10 @@ function Grid({ gridColumns }) {
     <StyledGrid gridColumns={gridColumns}>
       {
         gridItems.map((item, index) => {
-          return <TestDiv key={index}>
-            <div>{item.title}</div>
+          return <TestDiv key={item.id}>
+            <div>
+              <img src={item.img} alt={item.title} />
+            </div>
           </TestDiv>
         }
         )}
@@ -63,7 +78,6 @@ const StyledGrid = styled.div`
   
   width: 100%;
 
-  
 `
 
 const TestDiv = styled.div`
@@ -75,7 +89,7 @@ margin: 0;
   width: 32%;
   height: 400px;
   box-sizing: border-box;
-  background-color: blue; 
+  background-color: transparent; 
   margin: 5px;
   @media (max-width: 768px) {
     box-sizing: border-box;
