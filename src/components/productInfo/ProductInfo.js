7f18e10510e58c66ecfb0842ef4cbc0f2bffc534
samuelaparker <samuel.a.parker00@gmail.com>
2021-09-20 10:48:38 -0400

    add link to women's

--- src/components/productInfo/ProductInfo.js
+++ src/components/productInfo/ProductInfo.js
@@ -25,11 +25,15 @@ export default function ProductInfo({ productItems }) {
         
         <p>or 4 interest-free installments of $24.50 by <span><StyledAfterpayLogo src={AfterpayLogo} alt="afterpay logo"></StyledAfterpayLogo></span></p>
 
-        <div><StarFull /><StarFull /><StarFull /><StarFull /><StarHalf /> <StyledReviewLink to={``}>(1963)</StyledReviewLink></div>
+        <div><StyledStarFull /><StyledStarFull /><StyledStarFull /><StyledStarFull /><StarHalf /> <StyledReviewLink to={``}>(1963)</StyledReviewLink></div>
 
         <p>CLASSICS:</p><ColorSwatchMistWhiteSole /><NavyNightWhiteSole /><JetBlackWhiteSole /><JetBlackBlackSole /><KaikuraWhiteWhiteSole />
         <p>LIMITED EDITION:</p><CharcoalCharcoalSole /><ChalkWhiteSole /><ChomonixDarkGreySole /><HarvestWhiteSole /><BoughTaupeSole /><BlueRidgeWhiteSole /><HeatheredBlackSunnyGoldSole />
         <div><SolWhiteSole /><LilacWhiteSole /></div>
+
+        <div><p>Also available in <UnderLinedLink to={''}>Women's Sizes</UnderLinedLink></p></div>
+
+        
 
         </div>
     );
@@ -43,14 +47,20 @@ const StyledLink = styled(props => <Link {...props} />)`
     font-weight: bold;
     font-size: 11px;
     
+    
     &:hover {
     text-decoration: underline; 
 }
 `
+const UnderLinedLink = styled(StyledLink)`
+    text-decoration: underline;
+    font-family: 'Hurme Geometric Sans Bold';
+    
+`
 
 
 const StyledAfterpayLogo = styled.img`
-    height: 10px;
+    height: 12px;
     vertical-align: middle;
 `
 const StyledReviewLink = styled(props => <Link {...props} />)`
@@ -58,4 +68,8 @@ const StyledReviewLink = styled(props => <Link {...props} />)`
     text-decoration: underline;
     color: black;
     font-size: 12px;
+`
+
+const StyledStarFull = styled(StarFull)`
+    margin-right: 5px;
 `
